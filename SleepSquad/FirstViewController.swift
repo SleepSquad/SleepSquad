c86c07fe5a76ede2151d4827ba6c39792ec8c284
agustin <agustinmalo99@gmail.com>
2016-01-23 14:31:10 -0500

    Initial Commit

---
+++ SleepSquad/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SleepSquad
+//
+//  Created by Agustin Malo  on 1/23/16.
+//  Copyright © 2016 SleepSquad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SleepSquad/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SleepSquad
+//
+//  Created by Agustin Malo  on 1/23/16.
+//  Copyright © 2016 SleepSquad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
