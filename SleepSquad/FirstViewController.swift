73b4e80727b46854e609cea29d31ab365ff5ba0e
agustin <agustinmalo99@gmail.com>
2016-01-23 20:40:58 -0500

    NSUserDefaults

--- SleepSquad/FirstViewController.swift
+++ SleepSquad/FirstViewController.swift
@@ -12,6 +12,21 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var timeLeft: UILabel!
     
+    var defaults: NSUserDefaults = NSUserDefaults(suiteName: "group.com.SleepSquad.app")!
+    
+//    let dict = ["Monday": (hour: 8, min: 27, ampm: "AM"), "Tuesday": (hour: 8, min: 27, ampm: "AM"), "Wednesday": (hour: 8, min: 27, ampm: "AM"), "Thursday": (hour: 8, min: 27, ampm: "AM"), "Friday": (hour: 8, min: 27, ampm: "AM"), "Saturday": (hour: 8, min: 27, ampm: "AM"), "Sunday": (hour: 8, min: 27, ampm: "AM")]
+    
+    let dict =
+        ["Monday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Tuesday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Wednesday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Thursday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Friday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Saturday": ["hours": 7, "minutes": 4, "ampm": "AM"],
+        "Sunday": ["hours": 7, "minutes": 4, "ampm": "AM"]]
+    
+    
+    
     @IBAction func changeTimeLeft(sender: AnyObject) {
         
         timeLeft.text = "fsafdsbkdsgdgf"
@@ -20,7 +35,10 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        defaults.setObject(dict, forKey: "SavedDict")
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
